e842f3558123f39b276a3587c5c7bd1a0c0e661d
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-10-09 15:50:20 -0700

    Wire up some nav btns.

--- app/src/main/java/com/testmenudrawer/android/testmenudrawer/VinList.java
+++ app/src/main/java/com/testmenudrawer/android/testmenudrawer/VinList.java
@@ -3,6 +3,7 @@ package com.testmenudrawer.android.testmenudrawer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -80,18 +81,12 @@ public class VinList extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_scan) {
+            Log.i("Nav", "Scan");
+        } else if (id == R.id.nav_support) {
+            Log.i("Nav", "Support");
+        } else if (id == R.id.nav_user) {
+            Log.i("Nav", "User");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
