4f36a76e232895f9c7465a2fbb6097f7243b6097
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-10-12 17:58:26 -0700

    Unique titles for each page.

---
+++ app/src/main/java/com/testmenudrawer/android/testmenudrawer/UserActivity.kt
@@ -0,0 +1,112 @@
+package com.testmenudrawer.android.testmenudrawer
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v4.app.SupportActivity
+import android.util.Log
+import android.view.View
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+
+import com.testmenudrawer.android.testmenudrawer.VinList
+
+/**
+ * Created by mvalencia on 10/9/17.
+ */
+
+class UserActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //        getMenuInflater().inflate(R.menu.vin_list, menu);
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_vin_list) {
+            Log.i("Nav", "Scan")
+            goToVinListActivity()
+            //        } else if (id == R.id.nav_support) {
+            //            Log.i("Nav", "Support");
+            //            goToSupportActivity();
+        } else if (id == R.id.nav_user) {
+            Log.i("Nav", "User")
+            goToUserActivity()
+        }
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun goToVinListActivity() {
+        val intent = Intent(applicationContext, VinList::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToSupportActivity() {
+        //        Intent intent = new Intent(getApplicationContext(), SupportActivity.class);
+        //        startActivity(intent);
+    }
+
+    private fun goToUserActivity() {
+        val intent = Intent(applicationContext, UserActivity::class.java)
+        startActivity(intent)
+    }
+}
