ddba83fb226fad1143c051fc75dc4ae5f4c7d289
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-10-20 13:26:51 -0700

    Add vehicle detail page (nothing on it).

---
+++ app/src/main/java/com/testmenudrawer/android/testmenudrawer/VehicleDetailActivity.java
@@ -0,0 +1,16 @@
+package com.testmenudrawer.android.testmenudrawer;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mvalencia on 10/19/17.
+ */
+
+public class VehicleDetailActivity {
+
+
+}
