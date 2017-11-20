c8c800bdf3195b36a11e7dec649a2163eeba5530
Miguel Valencia <miguel.s.valencia@gmail.com>
2017-11-20 13:39:18 -0500

    Include arch. components and basic ViewModel.

---
+++ app/src/main/java/com/testmenudrawer/android/testmenudrawer/models/SupportViewModel.java
@@ -0,0 +1,15 @@
+package com.testmenudrawer.android.testmenudrawer.models;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by mvalencia on 11/20/17.
+ */
+
+public class SupportViewModel extends ViewModel {
+    // Tracks the score for Team A
+    public int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    public int scoreTeamB = 0;
+}
